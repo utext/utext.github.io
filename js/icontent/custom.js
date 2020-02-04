7b051041e64385b477c5fe13c93e2ac687829537
Nayden Gochev <nayden.gochev@novarto.com>
2020-02-04 17:34:49 +0200

    initial commit

---
+++ js/icontent/custom.js
@@ -0,0 +1,12 @@
+$(function(){ //document ready
+    console.log('jquery ready');
+    // $('.navbar-collapse').collapse('hide');
+    $('[data-toggle="popover"]').popover()
+});
+
+
+// console.log("TEST");
+//
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log("dom content loaded");
+// });
---
+++ precache-manifest.d057dccdc56b7ee1d42e5bfb9aeddc46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856bd74071ed3b9de9414a1d0d041dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d57ff6d4dbe69e9029",
+    "url": "/static/css/main.7a124fa3.chunk.css"
+  },
+  {
+    "revision": "858bd4830034e3dd51f9",
+    "url": "/static/js/2.39441a4d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.39441a4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28d57ff6d4dbe69e9029",
+    "url": "/static/js/main.6f2adc2f.chunk.js"
+  },
+  {
+    "revision": "b17bcf73d01f2d25784b",
+    "url": "/static/js/runtime-main.3f359fb0.js"
+  }
+]);
